ab98b261c2835ad87d07e7b13fbcd89a42a73355
Eunyoung Kim <snow.kim@jocoos.com>
2018-07-11 12:09:27 +0900

    etc: Remove unnecessary annotation

--- src/main/java/com/jocoos/mybeautip/goods/GoodsRepository.java
+++ src/main/java/com/jocoos/mybeautip/goods/GoodsRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface GoodsRepository extends CrudRepository<Goods, String> {
   
   @Query("select g from Goods g where g.updatedAt <= ?1 order by g.updatedAt desc")
