a020240f34e5d0b681dcc453febb16938e0916a3
Eunyoung Kim <snow.kim@jocoos.com>
2018-07-11 19:55:22 +0900

    etc: Remove unused annotation

--- src/main/java/com/jocoos/mybeautip/restapi/Response.java
+++ src/main/java/com/jocoos/mybeautip/restapi/Response.java
@@ -1,7 +1,5 @@
 package com.jocoos.mybeautip.restapi;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.jocoos.mybeautip.goods.Goods;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +7,6 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Data
-@JsonIgnoreProperties(ignoreUnknown = true)
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@NoArgsConstructor
 public class Response {
   private Integer code;
   private String message;
