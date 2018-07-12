54832fe13d415f155ab342544f570a295e13d5a7
Eunyoung Kim <snow.kim@jocoos.com>
2018-07-12 13:37:16 +0900

    feature: Modify category request path

--- src/main/java/com/jocoos/mybeautip/restapi/CategoryController.java
+++ src/main/java/com/jocoos/mybeautip/restapi/CategoryController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/1/goods/categories")
+@RequestMapping("/api/1/categories")
 public class CategoryController {
   @Autowired
   private CategoryService categoryService;
