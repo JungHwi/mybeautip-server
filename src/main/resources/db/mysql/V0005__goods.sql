55a55d282db2c571f73a741f68623622ea139ece
Eunyoung Kim <snow.kim@jocoos.com>
2018-07-17 11:54:09 +0900

    etc: Remove unused schema

--- src/main/resources/db/mysql/V0005__goods.sql
+++ src/main/resources/db/mysql/V0005__goods.sql
@@ -1,4 +1,3 @@
 ALTER TABLE `goods` CHANGE `updated_at` `modified_at` LONG;
 ALTER TABLE `goods` CHANGE `created_at` `created_at` DATETIME NOT NULL;
 ALTER TABLE `goods` CHANGE `modified_at` `modified_at` DATETIME DEFAULT NULL;
-ALTER TABLE `goods_categories` ADD `created_at` DATETIME NOT NULL AFTER `display_on_mobile`;
