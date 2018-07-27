6f314ec3f99f7a20efb7e6bea9889cacd68f548d
Eunyoung Kim <snow.kim@jocoos.com>
2018-07-27 15:41:31 +0900

    feature: Add stores and store likes

---
+++ src/main/java/com/jocoos/mybeautip/store/Store.java
@@ -0,0 +1,51 @@
+package com.jocoos.mybeautip.store;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@NoArgsConstructor
+@Data
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "stores")
+public class Store {
+
+  @Id
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column
+  private String description;
+
+  @Column
+  private String imageUrl;
+
+  @Column
+  private String thumbnailUrl;
+
+  @Column(nullable = false)
+  private Long likeCount = 0L;
+
+  @Column(nullable = false)
+  @CreatedDate
+  private Date createdAt;
+
+  @Column
+  @LastModifiedDate
+  private Date modifiedAt;
+
+  @Column
+  private Date deletedAt;
+
+  public Store(Long scmNo) {
+    this.setId(scmNo);
+  }
+}
