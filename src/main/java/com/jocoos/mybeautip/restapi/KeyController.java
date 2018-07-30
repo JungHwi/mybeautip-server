2b6082a1872990c681d39a57b1149abec812b4b2
Eunyoung Kim <snow.kim@jocoos.com>
2018-07-30 16:45:04 +0900

    feature: Add S3 access info retrieve API

--- src/main/java/com/jocoos/mybeautip/restapi/KeyController.java
+++ src/main/java/com/jocoos/mybeautip/restapi/KeyController.java
@@ -1,5 +1,14 @@
 package com.jocoos.mybeautip.restapi;
 
+import com.amazonaws.services.securitytoken.AWSSecurityTokenService;
+import com.amazonaws.services.securitytoken.model.Credentials;
+import com.amazonaws.services.securitytoken.model.GetSessionTokenRequest;
+import com.amazonaws.services.securitytoken.model.GetSessionTokenResult;
+import com.jocoos.mybeautip.exception.BadRequestException;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,19 +17,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
-
-import com.jocoos.mybeautip.exception.BadRequestException;
-
 @Slf4j
 @RestController
 @RequestMapping("/api/1")
 public class KeyController {
 
+  @Autowired
+  private AWSSecurityTokenService awsSTS;
+
   @Value("${mybeautip.addr.confirm-key}")
   private String confirmKey;
+
+  @Value("${mybeautip.aws.s3.bucket}")
+  private String bucketName;
+
+  @Value("${mybeautip.aws.s3.region}")
+  private String region;
+
+  @Value("${mybeautip.aws.sts.token-validity-duration-seconds}")
+  private String tokenValidityDurations;
+
 
   @GetMapping("/keys")
   public ResponseEntity<KeyInfo> getAddressConfirmKey(@RequestParam String name) {
@@ -28,15 +44,42 @@ public class KeyController {
 
     switch (name) {
       case "addr_confirm":
-        return new ResponseEntity<KeyInfo>(new KeyInfo(confirmKey), HttpStatus.OK);
+        return new ResponseEntity<>(new KeyInfo(confirmKey), HttpStatus.OK);
+
+      case "storage":
+        GetSessionTokenRequest request = new GetSessionTokenRequest();
+        request.withDurationSeconds(Integer.parseInt(tokenValidityDurations));
+        GetSessionTokenResult result = awsSTS.getSessionToken(request);
+        KeyInfo keyInfo = new KeyInfo(bucketName, region, result.getCredentials());
+        return new ResponseEntity<>(keyInfo, HttpStatus.OK);
+
       default:
         throw new BadRequestException("invalid request name - " + name);
     }
   }
 
-  @AllArgsConstructor
+  @NoArgsConstructor
   @Getter
   static class KeyInfo {
     private String confirmKey;
+    private String bucket;
+    private String region;
+    private String accessKeyId;
+    private String secretAccessKey;
+    private String sessionToken;
+    private Long expiration;
+
+    private KeyInfo(String confirmKey) {
+      this.confirmKey = confirmKey;
+    }
+
+    private KeyInfo(String bucket, String region, Credentials credentials) {
+      this.bucket = bucket;
+      this.region = region;
+      this.accessKeyId = credentials.getAccessKeyId();
+      this.secretAccessKey = credentials.getSecretAccessKey();
+      this.sessionToken = credentials.getSessionToken();
+      this.expiration = credentials.getExpiration().getTime();
+    }
   }
 }
