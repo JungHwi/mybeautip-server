f43bdca93cec329ee15a4ae3ed58f315d4b5d6f0
tekhun.jeong <tekhun.jeong@jocoos.com>
2018-08-16 15:21:06 +0900

    feature: Add jedis configs

---
+++ src/main/java/com/jocoos/mybeautip/config/RedisConfig.java
@@ -0,0 +1,49 @@
+package com.jocoos.mybeautip.config;
+
+import java.time.Duration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import lombok.Data;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+@Data
+@Configuration
+@ConfigurationProperties("redis")
+public class RedisConfig {
+
+  private String host;
+  private int port;
+  private String password;
+
+  @Bean
+  public ObjectMapper objectMapper() {
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+    return objectMapper;
+  }
+
+  @Bean
+  public JedisPool jedisPool() {
+    JedisPoolConfig poolConfig = new JedisPoolConfig();
+    poolConfig.setMaxTotal(128);
+    poolConfig.setMaxIdle(128);
+    poolConfig.setMinIdle(16);
+    poolConfig.setTestOnBorrow(true);
+    poolConfig.setTestOnReturn(true);
+    poolConfig.setTestWhileIdle(true);
+    poolConfig.setMinEvictableIdleTimeMillis(Duration.ofSeconds(60).toMillis());
+    poolConfig.setTimeBetweenEvictionRunsMillis(Duration.ofSeconds(30).toMillis());
+    poolConfig.setNumTestsPerEvictionRun(3);
+    poolConfig.setBlockWhenExhausted(true);
+
+    return new JedisPool(poolConfig, host, port, 3600, password);
+  }
+}
