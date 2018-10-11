1ab32f7b22fd40f21a77e4c5a475c460a5756cb1
Eunyoung Kim <snow.kim@jocoos.com>
2018-10-11 16:25:05 +0900

    feature: Modify cart items with shipping fee

--- src/main/java/com/jocoos/mybeautip/member/cart/CartRepository.java
+++ src/main/java/com/jocoos/mybeautip/member/cart/CartRepository.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
-  Optional<Cart> findByGoodsGoodsNoAndOptionNo(String goodsNo, Integer optionNo);
+  Optional<Cart> findByGoodsGoodsNoAndOptionOptionNo(String goodsNo, Integer optionNo);
 
   Optional<Cart> findByIdAndCreatedById(Long id, Long memberId);
 
--- src/main/java/com/jocoos/mybeautip/restapi/CartController.java
+++ src/main/java/com/jocoos/mybeautip/restapi/CartController.java
@@ -1,14 +1,10 @@
 package com.jocoos.mybeautip.restapi;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
-import java.util.*;
+import java.util.Optional;
 
-import org.springframework.beans.BeanUtils;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import lombok.AllArgsConstructor;
@@ -16,15 +12,18 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import com.jocoos.mybeautip.exception.BadRequestException;
 import com.jocoos.mybeautip.exception.MemberNotFoundException;
 import com.jocoos.mybeautip.exception.NotFoundException;
 import com.jocoos.mybeautip.goods.Goods;
-import com.jocoos.mybeautip.goods.GoodsInfo;
+import com.jocoos.mybeautip.goods.GoodsOption;
+import com.jocoos.mybeautip.goods.GoodsOptionRepository;
 import com.jocoos.mybeautip.goods.GoodsRepository;
-import com.jocoos.mybeautip.goods.GoodsService;
 import com.jocoos.mybeautip.member.MemberService;
 import com.jocoos.mybeautip.member.cart.Cart;
 import com.jocoos.mybeautip.member.cart.CartRepository;
+import com.jocoos.mybeautip.member.cart.CartService;
+import com.jocoos.mybeautip.store.Store;
 import com.jocoos.mybeautip.store.StoreRepository;
 
 @Slf4j
@@ -33,51 +32,75 @@ import com.jocoos.mybeautip.store.StoreRepository;
 public class CartController {
 
   private final MemberService memberService;
-  private final GoodsService goodsService;
+  private final CartService cartService;
   private final CartRepository cartRepository;
   private final GoodsRepository goodsRepository;
+  private final GoodsOptionRepository goodsOptionRepository;
   private final StoreRepository storeRepository;
 
   public CartController(MemberService memberService,
-                        GoodsService goodsService,
+                        CartService cartService,
                         CartRepository cartRepository,
                         GoodsRepository goodsRepository,
+                        GoodsOptionRepository goodsOptionRepository,
                         StoreRepository storeRepository) {
     this.memberService = memberService;
-    this.goodsService = goodsService;
+    this.cartService = cartService;
     this.cartRepository = cartRepository;
     this.goodsRepository = goodsRepository;
+    this.goodsOptionRepository = goodsOptionRepository;
     this.storeRepository = storeRepository;
   }
 
-  @PostMapping
-  public ResponseEntity<CartInfo> addCart(@Valid @RequestBody AddCartRequest request) {
-    Optional<Goods> optional = goodsRepository.findById(request.getGoodsNo());
-    Goods goods;
+  @GetMapping("/count")
+  public CartCountResponse getCartItemCount() {
+    return new CartCountResponse(cartRepository.countByCreatedById(memberService.currentMemberId()));
+  }
 
-    if (optional.isPresent()) {
-      goods = optional.get();
-    } else {
-      throw new NotFoundException("goods_not_found", "goods not found: " + request.getGoodsNo());
+  @GetMapping
+  public CartService.CartInfo getCartItemList() {
+    return cartService.getCartItemList();
+  }
+
+  @PostMapping
+  public CartService.CartInfo addCart(@Valid @RequestBody AddCartRequest request) {
+    Goods goods = goodsRepository.findById(request.getGoodsNo())
+      .orElseThrow(() -> new NotFoundException("goods_not_found", "goods not found: " + request.getGoodsNo()));
+
+    if ("y".equals(goods.getSoldOutFl())) {
+      throw new BadRequestException("goods_sold_out", "sold out: " + request.getGoodsNo());
     }
 
-    Optional<Cart> optionalCart = cartRepository.findByGoodsGoodsNoAndOptionNo(
+    GoodsOption option = null;
+    if ("y".equals(goods.getOptionFl())) {
+      option = goodsOptionRepository.findByGoodsNoAndOptionNo(Integer.parseInt(goods.getGoodsNo()), request.getOptionNo())
+        .orElseThrow(() -> new NotFoundException("goods_option_not_found", "goods option not found: " + request.getGoodsNo()));
+      if ("y".equals(goods.getStockFl()) && option.getStockCnt() < request.getQuantity()) { // 재고량에 따름
+        throw new BadRequestException("invalid_quantity", "goods option stock count is lower than quantity: " + request.getGoodsNo());
+      }
+    } else if (request.getOptionNo() != 0) {
+      throw new BadRequestException("goods_option_not_exist", "goods option not exist:" + request.getGoodsNo());
+    }
+
+    Store store = storeRepository.findById(goods.getScmNo())
+      .orElseThrow(() -> new NotFoundException("store_not_found", "store not found: " + goods.getScmNo()));
+
+    Optional<Cart> optionalCart = cartRepository.findByGoodsGoodsNoAndOptionOptionNo(
       request.getGoodsNo(), request.getOptionNo());
     Cart cart;
     if (optionalCart.isPresent()) { // Update quantity
       cart = optionalCart.get();
       cart.setQuantity(request.getQuantity());
     } else {  // Insert new item
-      cart = new Cart(goods, request.getOptionNo(), request.getQuantity());
+      cart = new Cart(goods, option, store, request.getQuantity());
     }
-    Cart result = cartRepository.save(cart);
-    return new ResponseEntity<>(
-      new CartInfo(result, goodsService.generateGoodsInfo(result.getGoods())), HttpStatus.OK);
+    cartRepository.save(cart);
+    return cartService.getCartItemList();
   }
 
   @PatchMapping("{id}")
-  public ResponseEntity<CartInfo> updateCart(@PathVariable Long id,
-                                             @Valid @RequestBody UpdateCartRequest request) {
+  public CartService.CartInfo updateCart(@PathVariable Long id,
+                                         @Valid @RequestBody UpdateCartRequest request) {
     Long memberId = memberService.currentMemberId();
     if (memberId == null) {
       throw new MemberNotFoundException("Login required");
@@ -88,69 +111,33 @@ public class CartController {
 
     if (optional.isPresent()) {
       cart = optional.get();
-      cart.setQuantity(request.getQuantity());
+      if (request.getQuantity() != null && request.getQuantity() > 0) {
+        cart.setQuantity(request.getQuantity());
+      }
+      if (request.getChecked() != null) {
+        cart.setChecked(request.getChecked());
+      }
     } else {
       throw new NotFoundException("cart_item_not_found", "cart item not found, id: " + id);
     }
-    Cart result = cartRepository.save(cart);
-    return new ResponseEntity<>(
-      new CartInfo(result, goodsService.generateGoodsInfo(result.getGoods())), HttpStatus.OK);
+
+    cart.setModifiedAt(cart.getModifiedAt()); // Not change modifiedAt
+    cartRepository.save(cart);
+    return cartService.getCartItemList();
   }
 
   @DeleteMapping("{id}")
-  public void removeCart(@PathVariable Long id) {
-    Long memberId = memberService.currentMemberId();
-    if (memberId == null) {
-      throw new MemberNotFoundException("Login required");
-    }
-
-    Optional<Cart> optional = cartRepository.findByIdAndCreatedById(id, memberId);
-
+  public CartService.CartInfo removeCart(@PathVariable Long id) {
+    Optional<Cart> optional = cartRepository.findByIdAndCreatedById(id, memberService.currentMemberId());
     if (optional.isPresent()) {
       cartRepository.deleteById(id);
     } else {
       throw new NotFoundException("cart_item_not_found", "cart item not found, id: " + id);
     }
+
+    return cartService.getCartItemList();
   }
 
-  @GetMapping("/count")
-  public CartCountResponse getCartItemCount() {
-    Long memberId = memberService.currentMemberId();
-    if (memberId == null) {
-      throw new MemberNotFoundException("Login required");
-    }
-    return new CartCountResponse(cartRepository.countByCreatedById(memberId));
-  }
-
-  @GetMapping
-  public List<CartItemGroup> getCartItems() {
-    Long memberId = memberService.currentMemberId();
-    List<Cart> allCartItems = cartRepository.findAllByCreatedByIdOrderByModifiedAtDesc(memberId);
-
-    Map<Integer, List<CartInfo>> map = new HashMap<>();
-    List<Integer> orderedScmNo = new ArrayList<>();
-    List<CartInfo> list;
-
-    for (Cart cart : allCartItems) {
-      if (map.containsKey(cart.getScmNo())) {
-        list = map.get(cart.getScmNo());
-      } else {
-        list = new ArrayList<>();
-      }
-      list.add(new CartInfo(cart, goodsService.generateGoodsInfo(cart.getGoods())));
-      map.put(cart.getScmNo(), list);
-      if (!orderedScmNo.contains(cart.getScmNo())) {
-        orderedScmNo.add(cart.getScmNo());
-      }
-    }
-
-    List<CartItemGroup> result = new ArrayList<>();
-    for (int scmNo : orderedScmNo) {
-      result.add(new CartItemGroup(scmNo, storeRepository.getOne(scmNo).getName(), map.get(scmNo)));
-    }
-
-    return result;
-  }
 
   @Data
   @NoArgsConstructor
@@ -162,47 +149,19 @@ public class CartController {
     private int optionNo;
 
     @NotNull
-    @Max(100)
     private int quantity;
   }
 
   @Data
   @NoArgsConstructor
   private static class UpdateCartRequest {
-    @NotNull
-    @Max(100)
-    private int quantity;
+    private Boolean checked;
+    private Integer quantity;
   }
 
   @Data
   @AllArgsConstructor
   private static class CartCountResponse {
     private Integer count;
-  }
-
-  @Data
-  private static class CartItemGroup {
-    private Integer scmNo;
-    private String storeName;
-    private List<CartInfo> content;
-
-    CartItemGroup(Integer scmNo, String storeName, List<CartInfo> content) {
-      this.scmNo = scmNo;
-      this.storeName = storeName;
-      this.content = content;
-    }
-  }
-
-  @Data
-  private static class CartInfo {
-    private Long id;
-    private GoodsInfo goods;
-    private Integer optionNo;
-    private Integer quantity;
-
-    CartInfo(Cart cart, GoodsInfo goods) {
-      BeanUtils.copyProperties(cart, this);
-      this.goods = goods;
-    }
   }
 }
