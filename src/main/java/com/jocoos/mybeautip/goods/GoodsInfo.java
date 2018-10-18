dcd59c87823441247c67d156ec368146029bb735
Eunyoung Kim <snow.kim@jocoos.com>
2018-10-18 11:09:01 +0900

    etc: Add goodsInfo properties(total_stock, sales_unit, min_order_cnt, max_order_cnt)

--- src/main/java/com/jocoos/mybeautip/goods/GoodsInfo.java
+++ src/main/java/com/jocoos/mybeautip/goods/GoodsInfo.java
@@ -22,15 +22,18 @@ public class GoodsInfo {
   private String goodsNo;  // 상품번호
   private String goodsNm;  // 상품명
   private String cateCd;  //  대표 카테고리
-  private String totalStock;  // 상품 재고
-  private String stockFl;  // 판매재고 (n=무한정판매, y=재고량에 따름)
   private String soldOutFl;  // 품절상태 (n= 정상, y=품절(수동))
+  private String stockFl;  // 판매재고 (n=무한정판매, y=재고량에 따름)
+  private Integer totalStock;  // 상품 재고
+  private Integer salesUnit;  // 묶음주문 단위
+  private Integer minOrderCnt;  // 최소 구매수량, 최대 구매수량 미입력시 적용불가
+  private Integer maxOrderCnt;  // 최대 구매 수량, 최소 구매수량 미입력시 적용불가
   private String goodsDiscountFl;  // 상품 할인 설정 ( y=사용함, n=사용안함)
-  private int goodsDiscount;  // 상품 할인 값
+  private Integer goodsDiscount;  // 상품 할인 값
   private String goodsDiscountUnit;  // 상품 할인 단위 ( percent=%, price=원)
-  private int goodsPrice;  // 판매가
-  private int fixedPrice;  // 정가
-  private URL listImageData;  // 썸네일 이미지 정보
+  private Integer goodsPrice;  // 판매가
+  private Integer fixedPrice;  // 정가
+  private String listImageData;  // 썸네일 이미지 정보
   private Integer scmNo;
   private Integer likeCount;
   private Long likeId;
