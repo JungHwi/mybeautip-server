cd6df4246fb3ada689b67d568692dadcac677c90
Eunyoung Kim <snow.kim@jocoos.com>
2018-10-19 10:39:19 +0900

    fix: Fix to calculate area-shipping fee

--- src/main/java/com/jocoos/mybeautip/member/address/AddressService.java
+++ src/main/java/com/jocoos/mybeautip/member/address/AddressService.java
@@ -31,7 +31,7 @@ public class AddressService {
   }
 
   public Address insert(Address address) {
-    address.setAreaShipping(getAreaShipping(address.getRoadAddrPart1()));
+    address.setAreaShipping(calculateAreaShipping(address.getRoadAddrPart1()));
     return addressRepository.save(address);
   }
 
@@ -41,7 +41,7 @@ public class AddressService {
     if (optional.isPresent()) {
       Address address = optional.get();
       BeanUtils.copyProperties(update, address);
-      address.setAreaShipping(getAreaShipping(address.getRoadAddrPart1()));
+      address.setAreaShipping(calculateAreaShipping(address.getRoadAddrPart1()));
       return addressRepository.save(address);
     } else {
       throw new NotFoundException("address not found", "address not found");
@@ -59,64 +59,56 @@ public class AddressService {
       throw new NotFoundException("address not found", "address not found");
     }
   }
-
-  private int getAreaShipping(String roadAddress1) {
+  
+  
+  private int calculateAreaShipping(String roadAddress1) {
     if (roadAddress1.startsWith("제주")) {
       return deliveryChargeAreaRepository.findByArea("제주시특별자치도").map(DeliveryChargeArea::getPrice).orElse(0);
     }
-
+    
     if (StringUtils.startsWith(roadAddress1, "인천")) {
-      if (StringUtils.startsWithAny(roadAddress1, "인천광역시 중구", "인천광역시 강화군", "인천광역시 옹진군")) {
-        return deliveryChargeAreaRepository.findByArea(roadAddress1).map(DeliveryChargeArea::getPrice).orElse(0);
-      } else {
-        return 0;
-      }
+      return StringUtils.startsWithAny(roadAddress1, "인천광역시 중구", "인천광역시 강화군", "인천광역시 옹진군")
+          ? getAreaShipping(roadAddress1) : 0;
     }
 
     if (StringUtils.startsWith(roadAddress1, "부산")) {
-      if (StringUtils.startsWith(roadAddress1, "부산광역시 강서구")) {
-        return deliveryChargeAreaRepository.findByArea(roadAddress1).map(DeliveryChargeArea::getPrice).orElse(0);
-      } else {
-        return 0;
-      }
+      return StringUtils.startsWith(roadAddress1, "부산광역시 강서구")
+          ? getAreaShipping(roadAddress1) : 0;
     }
 
     if (StringUtils.startsWith(roadAddress1, "경상남도")) {
-      if (StringUtils.startsWithAny(roadAddress1,"경상남도 사천시", "경상남도 통영시")) {
-        return deliveryChargeAreaRepository.findByArea(roadAddress1).map(DeliveryChargeArea::getPrice).orElse(0);
-      } else {
-        return 0;
-      }
+      return StringUtils.startsWithAny(roadAddress1,"경상남도 사천시", "경상남도 통영시")
+          ? getAreaShipping(roadAddress1) : 0;
     }
 
     if (StringUtils.startsWith(roadAddress1, "경상북도")) {
-      if (StringUtils.startsWith(roadAddress1, "경상북도 울릉군")) {
-        return deliveryChargeAreaRepository.findByArea(roadAddress1).map(DeliveryChargeArea::getPrice).orElse(0);
-      } else {
-        return 0;
-      }
+      return StringUtils.startsWith(roadAddress1, "경상북도 울릉군")
+          ? getAreaShipping(roadAddress1) : 0;
     }
 
     if (StringUtils.startsWith(roadAddress1, "충청남도")) {
-      if (StringUtils.startsWithAny(roadAddress1, "충청남도 당진시", "충청남도 보령시", "충청남도 태안군")) {
-        return deliveryChargeAreaRepository.findByArea(roadAddress1).map(DeliveryChargeArea::getPrice).orElse(0);
-      } else {
-        return 0;
-      }
+      return StringUtils.startsWithAny(roadAddress1, "충청남도 당진시", "충청남도 보령시", "충청남도 태안군")
+          ? getAreaShipping(roadAddress1) : 0;
     }
 
     if (StringUtils.startsWith(roadAddress1, "전라북도")) {
-      if (StringUtils.startsWithAny(roadAddress1, "전라북도 군산시", "전라북도 부안군")) {
-        return deliveryChargeAreaRepository.findByArea(roadAddress1).map(DeliveryChargeArea::getPrice).orElse(0);
-      } else {
-        return 0;
-      }
+      return StringUtils.startsWithAny(roadAddress1, "전라북도 군산시", "전라북도 부안군")
+          ? getAreaShipping(roadAddress1) : 0;
     }
 
     if (StringUtils.startsWith(roadAddress1, "전라남도")) {
-      return deliveryChargeAreaRepository.findByArea(roadAddress1).map(DeliveryChargeArea::getPrice).orElse(0);
+      return getAreaShipping(roadAddress1);
     }
 
     return 0;
+  }
+  
+  private int getAreaShipping(String roadAddress1) {
+    String[] part = roadAddress1.split(" ");
+    if (part.length < 4) {
+      return 0;
+    }
+    return deliveryChargeAreaRepository.findByPart1AndPart2AndPart3AndPart4(part[0], part[1], part[2], part[3])
+        .map(DeliveryChargeArea::getPrice).orElse(0);
   }
 }
