710e65f095b101245a51cc985b714db77d901648
Eunyoung Kim <snow.kim@jocoos.com>
2018-10-22 17:50:47 +0900

    etc: Add condition price max/min in coupon

--- src/main/resources/db/mysql/V0062__alter_goods.sql
+++ src/main/resources/db/mysql/V0062__alter_goods.sql
@@ -1,3 +1,7 @@
 ALTER TABLE `goods` MODIFY COLUMN `delivery_sno` INT(10) NOT NULL DEFAULT 0;
 ALTER TABLE `goods` ADD COLUMN `delivery_method` VARCHAR(40) NOT NULL DEFAULT '' AFTER `delivery_sno`;
 ALTER TABLE `goods` ADD COLUMN `delivery_fix_fl` VARCHAR(6) NOT NULL DEFAULT '' AFTER `delivery_sno`;
+ALTER TABLE `goods` ADD COLUMN `deleted_at` DATETIME(3) AFTER `modified_at`;
+
+ALTER TABLE `coupons` ADD COLUMN `condition_price_min` INT UNSIGNED DEFAULT NULL AFTER `condition_price`;
+ALTER TABLE `coupons` ADD COLUMN `condition_price_max` INT UNSIGNED DEFAULT NULL AFTER `condition_price`;
