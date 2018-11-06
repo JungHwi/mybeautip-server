a8f177e05eeb13714bcbf4dba36083ae29a4b5ca
tekhun.jeong <tekhun.jeong@jocoos.com>
2018-11-06 21:10:08 +0900

    feature: Add recommend projection

---
+++ src/main/java/com/jocoos/mybeautip/admin/GoodsRecommendationExcerpt.java
@@ -0,0 +1,25 @@
+package com.jocoos.mybeautip.admin;
+
+import java.util.Date;
+
+import org.springframework.data.rest.core.config.Projection;
+
+import com.jocoos.mybeautip.goods.Goods;
+import com.jocoos.mybeautip.member.Member;
+import com.jocoos.mybeautip.recommendation.GoodsRecommendation;
+
+@Projection(name = "recommendation_goods", types = GoodsRecommendation.class)
+public interface GoodsRecommendationExcerpt {
+
+  Goods getGoods();
+
+  int getSeq();
+
+  Date getCreatedAt();
+
+  Date getStartedAt();
+
+  Date getEndedAt();
+
+  Member getCreatedBy();
+}
