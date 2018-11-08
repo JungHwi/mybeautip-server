3d7f22c83ec19302a7fc09a167e4202cfa565745
Eunyoung Kim <snow.kim@jocoos.com>
2018-11-08 14:31:21 +0900

    etc: Add exposeId classes for rest data

--- src/main/java/com/jocoos/mybeautip/config/RepositoryConfig.java
+++ src/main/java/com/jocoos/mybeautip/config/RepositoryConfig.java
@@ -1,5 +1,7 @@
 package com.jocoos.mybeautip.config;
 
+import com.jocoos.mybeautip.goods.*;
+import com.jocoos.mybeautip.store.Store;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
@@ -15,6 +17,8 @@ public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
 
   @Override
   public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-    config.exposeIdsFor(Coupon.class, Post.class, Banner.class, Member.class, Order.class);
+    config.exposeIdsFor(Coupon.class, Post.class, Banner.class, Member.class, Order.class,
+        Category.class, DeliveryCharge.class, DeliveryChargeDetail.class, Store.class,
+        Goods.class, GoodsOption.class);
   }
 }
