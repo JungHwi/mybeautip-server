3658e5af2666b2587d8cd802581b79e68b6167f3
tekhun.jeong <tekhun.jeong@jocoos.com>
2018-11-08 17:24:00 +0900

    feature: Add member reports projection

--- src/main/java/com/jocoos/mybeautip/member/report/ReportRepository.java
+++ src/main/java/com/jocoos/mybeautip/member/report/ReportRepository.java
@@ -2,8 +2,9 @@ package com.jocoos.mybeautip.member.report;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ReportRepository extends CrudRepository<Report, Long> {
+public interface ReportRepository extends JpaRepository<Report, Long> {
   Optional<Report> findByMeIdAndYouId(long me, long you);
 }
