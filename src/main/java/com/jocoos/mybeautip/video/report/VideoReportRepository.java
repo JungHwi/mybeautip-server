e0071a4066548925f7480708114d672b3a443c9e
tekhun.jeong <tekhun.jeong@jocoos.com>
2018-11-12 16:02:48 +0900

    feature: Add reported motd projections

--- src/main/java/com/jocoos/mybeautip/video/report/VideoReportRepository.java
+++ src/main/java/com/jocoos/mybeautip/video/report/VideoReportRepository.java
@@ -2,8 +2,12 @@ package com.jocoos.mybeautip.video.report;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VideoReportRepository extends JpaRepository<VideoReport, Long> {
   Optional<VideoReport> findByVideoIdAndCreatedById(Long videoId, Long createdById);
+
+  Page<VideoReport> findByVideoTypeAndVideoState(String type, String state, Pageable pageable);
 }
