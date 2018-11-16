1a396e49dd550761d6a152b8a28a21e3d8abf1c7
Eunyoung Kim <snow.kim@jocoos.com>
2018-11-17 08:44:59 +0900

    fix: Add transactional annotation and append logs

--- src/main/java/com/jocoos/mybeautip/member/PostProcessService.java
+++ src/main/java/com/jocoos/mybeautip/member/PostProcessService.java
@@ -1,12 +1,16 @@
 package com.jocoos.mybeautip.member;
 
+import javax.transaction.Transactional;
+
 import com.jocoos.mybeautip.member.cart.CartRepository;
 import com.jocoos.mybeautip.member.following.FollowingRepository;
 import com.jocoos.mybeautip.video.VideoRepository;
 import com.jocoos.mybeautip.video.VideoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class PostProcessService {
   
@@ -26,20 +30,25 @@ public class PostProcessService {
   }
   
   @Async
+  @Transactional
   public void deleteMember(Member member) {
     // 1. Delete member's all videos (using videoService)
     // 2. Delete all followings related members (followings/followers)
     // 3. Delete all cart items created by member
     // TODO: Delete garbage data depends on policy(order, addresses, account, likes, views, blocks, reports, comments)
-    
+
+    log.debug("Member {} deleted: video will be deleted", member.getId());
     videoRepository.findByMemberAndDeletedAtIsNull(member)
         .forEach(video -> {
           videoService.deleteVideo(member.getId(), video.getVideoKey());
+          log.debug("{} video deleted", video.getId());
         });
-    
+
+    log.debug("Member {} deleted: followings will be deleted", member.getId());
     followingRepository.findByMemberMeIdOrMemberYouId(member.getId(), member.getId())
         .forEach(followingRepository::delete);
-    
+
+    log.debug("Member {} deleted: cart items will be deleted", member.getId());
     cartRepository.findByCreatedById(member.getId())
         .forEach(cartRepository::delete);
   }
