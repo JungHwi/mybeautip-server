806c17e57f8d19738d80d139e00f28124bb97fea
Eunyoung Kim <snow.kim@jocoos.com>
2019-01-11 17:40:48 +0900

    etc: Add pointcut expression for Video view recoding when joinWatch

--- src/main/java/com/jocoos/mybeautip/recoding/ViewRecodingAspect.java
+++ src/main/java/com/jocoos/mybeautip/recoding/ViewRecodingAspect.java
@@ -81,7 +81,8 @@ public class ViewRecodingAspect {
     }
   }
 
-  @AfterReturning(value = "execution(* com.jocoos.mybeautip.restapi.VideoController.addView(..))",
+  @AfterReturning(value = "execution(* com.jocoos.mybeautip.restapi.VideoController.addView(..)) " +
+                          "|| execution(* com.jocoos.mybeautip.restapi.VideoController.joinWatch(..))",
     returning = "result")
   public void onAfterReturningVideoAddViewCountHandler(JoinPoint joinPoint, Object result) {
     log.debug("joinPoint: {}", joinPoint.toLongString());
