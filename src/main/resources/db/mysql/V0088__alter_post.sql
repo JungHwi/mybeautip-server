9e8d9c9305ae48b4ad95832d0154a9e2aa846436
tekhun.jeong <tekhun.jeong@jocoos.com>
2019-01-15 13:38:52 +0900

    fix: Fix flyway number

--- src/main/resources/db/mysql/V0088__alter_post.sql
+++ src/main/resources/db/mysql/V0088__alter_post.sql
@@ -7,7 +7,7 @@ ALTER TABLE `posts` ADD COLUMN `ended_at` DATETIME DEFAULT NULL AFTER `started_a
 UPDATE `posts` SET opened = 1, started_at = now(), ended_at = '2019-02-28 23:59:59';
 
 
--- DELETE FROM flyway_schema_history WHERE installed_rank = 89;
+-- DELETE FROM flyway_schema_history WHERE installed_rank = 88;
 -- ALTER TABLE `posts` DROP COLUMN `opened`;
 -- ALTER TABLE `posts` DROP COLUMN `started_at`;
 -- ALTER TABLE `posts` DROP COLUMN `ended_at`;
