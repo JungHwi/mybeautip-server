156a794cdf2542d7e262ac285d79c5eb317b6e77
Eunyoung Kim <snow.kim@jocoos.com>
2019-01-25 14:00:18 +0900

    etc: Modify to use request body

--- src/main/java/com/jocoos/mybeautip/admin/AdminCommentController.java
+++ src/main/java/com/jocoos/mybeautip/admin/AdminCommentController.java
@@ -4,11 +4,14 @@ import java.util.Optional;
 
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
+import com.jocoos.mybeautip.exception.BadRequestException;
 import com.jocoos.mybeautip.exception.NotFoundException;
 import com.jocoos.mybeautip.member.comment.CommentRepository;
 import com.jocoos.mybeautip.member.comment.CommentService;
@@ -27,13 +30,28 @@ public class AdminCommentController {
     this.commentRepository = commentRepository;
   }
   
-  @PatchMapping("/{id:.+}/lock")
-  public void lockComment(@PathVariable Long id) {
-    commentRepository.findById(id)
-        .map(comment -> {
-          commentService.lockComment(comment);
-          return Optional.empty();
-        })
-        .orElseThrow(() -> new NotFoundException("comment_not_found", "Comment not found, id: " + id));
+  @PatchMapping("/{id:.+}")
+  public void lockComment(@PathVariable Long id,
+                          @RequestBody UpdateCommentRequest request) {
+    log.debug("request: {}", request);
+  
+    if (request != null && request.getLocked()) {
+      commentRepository.findById(id)
+          .map(comment -> {
+            if (comment.getLocked()) {
+              throw new BadRequestException("already_locked", "Comment already locked");
+            }
+            commentService.lockComment(comment);
+            return Optional.empty();
+          })
+          .orElseThrow(() -> new NotFoundException("comment_not_found", "Comment not found, id: " + id));
+    } else {
+      throw new BadRequestException("invalid_request_body", "Invalid request body");
+    }
+  }
+  
+  @Data
+  public static class UpdateCommentRequest {
+    private Boolean locked;
   }
 }
