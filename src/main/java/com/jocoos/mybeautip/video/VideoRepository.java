4131ce70d278294dff138f4be09e94a8cc157fe7
tekhun.jeong <tekhun.jeong@jocoos.com>
2019-01-29 21:24:42 +0900

    feature: Add recently videos for admin

--- src/main/java/com/jocoos/mybeautip/video/VideoRepository.java
+++ src/main/java/com/jocoos/mybeautip/video/VideoRepository.java
@@ -128,4 +128,6 @@ public interface VideoRepository extends JpaRepository<Video, Long> {
   Page<Video> findByTypeAndStateInAndReportCountGreaterThanAndDeletedAtIsNull(String type, Collection<String> state, Long reportCount, Pageable pageable);
 
   Page<Video> findByTypeAndStateInAndReportCountGreaterThanAndDeletedAtIsNotNull(String type, Collection<String> state, Long reportCount, Pageable pageable);
+
+  Page<Video> findByTypeAndStateInAndAndCreatedAtBetweenAndDeletedAtIsNull(String type, Collection<String> state, Date from, Date now, Pageable pageable);
 }
