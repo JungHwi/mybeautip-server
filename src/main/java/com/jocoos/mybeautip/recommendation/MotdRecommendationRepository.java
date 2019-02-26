194413c99fcfeb15d3c37b2df130e7f74d164480
Eunyoung Kim <snow.kim@jocoos.com>
2019-02-26 16:56:43 +0900

    fix: Modify recommended_motds retrieve condition

--- src/main/java/com/jocoos/mybeautip/recommendation/MotdRecommendationRepository.java
+++ src/main/java/com/jocoos/mybeautip/recommendation/MotdRecommendationRepository.java
@@ -3,7 +3,6 @@ package com.jocoos.mybeautip.recommendation;
 import java.util.Date;
 import java.util.Optional;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,7 +11,7 @@ public interface MotdRecommendationRepository extends JpaRepository<MotdRecommen
 
   Optional<MotdRecommendation> findByVideoId(Long id);
 
-  Slice<MotdRecommendation> findByVideoCreatedAtBefore(Date createdAt, Pageable pageable);
+  Slice<MotdRecommendation> findByVideoVisibilityAndVideoDeletedAtIsNullAndVideoStateNotAndCreatedAtBefore(String visibility, String state, Date createdAt, Pageable pageable);
 
   Slice<MotdRecommendation> findByBaseIdAndStartedAtBeforeAndEndedAtAfterAndVideoDeletedAtIsNull(Long baseId, Date statedAt, Date endedAt, Pageable pageable);
 
