669da353d98b540082dc7a0854d5c7e4c33200a7
tekhun.jeong <tekhun.jeong@jocoos.com>
2022-01-05 14:25:44 +0900

    Fix permit all api for iamport webhook

--- src/main/java/com/jocoos/mybeautip/config/SecurityConfig.java
+++ src/main/java/com/jocoos/mybeautip/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -21,6 +22,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import static org.springframework.http.HttpMethod.OPTIONS;
+import static org.springframework.http.HttpMethod.POST;
 
 @Order(-10)
 @Configuration
@@ -57,6 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .and()
         .requestMatchers()
         .antMatchers(OPTIONS,"/api/**")
+        .antMatchers(POST,"/api/1/payments/notification")
         .and()
         .authorizeRequests()
         .antMatchers(OPTIONS,"/api/**").permitAll()
