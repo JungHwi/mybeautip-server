d00b1ceeb7274790968ced81814d36f030b1f55f
breeze <breeze@jocoos.com>
2022-05-10 09:56:08 +0900

    conflict 수정

--- src/main/java/com/jocoos/mybeautip/restapi/AuthController.java
+++ src/main/java/com/jocoos/mybeautip/restapi/AuthController.java
@@ -8,6 +8,7 @@ import com.jocoos.mybeautip.security.SocialLoginService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
