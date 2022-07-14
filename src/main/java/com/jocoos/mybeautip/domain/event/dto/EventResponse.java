1302de187ac95b6b26d2fcdf9302820446a3336c
breeze <breeze@jocoos.com>
2022-07-14 11:19:34 +0900

    EventResponse 에 description 추가

--- src/main/java/com/jocoos/mybeautip/domain/event/dto/EventResponse.java
+++ src/main/java/com/jocoos/mybeautip/domain/event/dto/EventResponse.java
@@ -18,6 +18,7 @@ public class EventResponse {
     private EventType type;
     private EventStatus status;
     private String title;
+    private String description;
     private String imageUrl;
     private String shareSquareImageUrl;
     private String shareRectangleImageUrl;
