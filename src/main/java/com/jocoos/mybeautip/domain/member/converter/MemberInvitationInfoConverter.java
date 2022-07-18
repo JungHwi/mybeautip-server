eb51de6a9c97a45dbc8c50c3363a1a060c8fae50
김재훈 <kjh96@jocoos.com>
2022-07-18 16:24:15 +0900

    MYBEAUTIP269 - [update] 친구 초대 정보 API DB정보 단순화
    이미지 URL의 경우 DB에는 전체 URL 주소가 아닌 파일명만 들어가도록 변경
    
    ImageUrlConvertUtil를 static하게 유지하고 싶어 @Value를 setter로 주입하는 방식을 선택

--- src/main/java/com/jocoos/mybeautip/domain/member/converter/MemberInvitationInfoConverter.java
+++ src/main/java/com/jocoos/mybeautip/domain/member/converter/MemberInvitationInfoConverter.java
@@ -2,9 +2,24 @@ package com.jocoos.mybeautip.domain.member.converter;
 
 import com.jocoos.mybeautip.domain.member.dto.MemberInvitationInfoResponse;
 import com.jocoos.mybeautip.domain.member.persistence.domain.MemberInvitationInfo;
-import org.mapstruct.Mapper;
+import com.jocoos.mybeautip.global.util.ImageUrlConvertUtil;
+import org.mapstruct.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static com.jocoos.mybeautip.global.code.UrlDirectory.SHARE;
+import static com.jocoos.mybeautip.global.util.ImageUrlConvertUtil.toUrl;
 
 @Mapper(componentModel = "spring")
 public interface MemberInvitationInfoConverter {
+
+    String CONVERT_TO_URL = "convertToUrl";
+
+    @Mapping(source = "shareSquareImageUrl", target = "shareSquareImageUrl", qualifiedByName = CONVERT_TO_URL)
+    @Mapping(source = "shareRectangleImageUrl", target = "shareRectangleImageUrl", qualifiedByName = CONVERT_TO_URL)
     MemberInvitationInfoResponse convertToResponse(MemberInvitationInfo info);
+
+    @Named(CONVERT_TO_URL)
+    default String convertToUrl(String filename) {
+        return toUrl(filename, SHARE);
+    }
 }
---
+++ src/main/java/com/jocoos/mybeautip/global/code/UrlDirectory.java
@@ -0,0 +1,13 @@
+package com.jocoos.mybeautip.global.code;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum UrlDirectory implements CodeValue {
+
+    SHARE("share/");
+
+    private final String description;
+}
