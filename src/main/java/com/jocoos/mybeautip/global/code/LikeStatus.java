888882db6d18e2f963031fc5b52060911b1df4e5
KimJaeHun <kjh96@jocoos.com>
2022-08-04 16:34:08 +0900

    MYBEAUTIP286 - [update] 실제 데이터 삭제 아닌 status 변경으로 수정

---
+++ src/main/java/com/jocoos/mybeautip/global/code/LikeStatus.java
@@ -0,0 +1,14 @@
+package com.jocoos.mybeautip.global.code;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum LikeStatus implements CodeValue {
+
+    LIKE("좋아요"),
+    UNLIKE("좋아요 취소");
+
+    private final String description;
+}
--- src/main/java/com/jocoos/mybeautip/video/VideoLike.java
+++ src/main/java/com/jocoos/mybeautip/video/VideoLike.java
@@ -1,6 +1,7 @@
 package com.jocoos.mybeautip.video;
 
 import com.jocoos.mybeautip.audit.MemberAuditable;
+import com.jocoos.mybeautip.global.code.LikeStatus;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -21,7 +22,20 @@ public class VideoLike extends MemberAuditable {
     @JoinColumn(name = "video_id")
     private Video video;
 
+    @Column
+    @Enumerated(EnumType.STRING)
+    private LikeStatus status;
+
     public VideoLike(Video video) {
         this.video = video;
+        this.status = LikeStatus.LIKE;
+    }
+
+    public void like() {
+        this.status = LikeStatus.LIKE;
+    }
+
+    public void unlike() {
+        this.status = LikeStatus.UNLIKE;
     }
 }
