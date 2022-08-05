b9f29874a3f5aa4b1aa1ecc08379d147084446d1
breeze <breeze@jocoos.com>
2022-08-05 13:57:58 +0900

    Community Category Table 생성

---
+++ src/main/resources/db/mysql/V0159__create_community_category.sql
@@ -0,0 +1,12 @@
+create table community_category (
+    id bigint auto_increment comment '커뮤니티 카테고리 아이디' primary key,
+    type varchar(20) not null comment '카테고리 구분',
+    sort int not null comment '정렬순서',
+    title varchar(20) not null comment '카테고리 제목',
+    description varchar(100) not null comment '카테고리 설명',
+    hint varchar(100) not null comment '카테고리 힌트'
+) comment '커뮤니티 카테고리 정보' charset = utf8mb4;
+
+# drop table community_category;
+#
+# delete from flyway_schema_history where version = '0159';
