8c8ee9c01ad05c65c80c04900a3ce89c91be801e
KimJaeHun <kjh96@jocoos.com>
2022-08-08 11:19:05 +0900

    MYBEAUTIP444 - [style] rest doc json 줄바꿈 적용

--- src/test/java/com/jocoos/mybeautip/global/config/restdoc/RestDocsTestSupport.java
+++ src/test/java/com/jocoos/mybeautip/global/config/restdoc/RestDocsTestSupport.java
@@ -12,13 +12,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.MockMvcOperationPreprocessorsConfigurer;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 
 @Disabled
 @Import(RestDocsConfig.class)
@@ -40,8 +43,13 @@ public class RestDocsTestSupport {
     @BeforeEach
     void setUp(final WebApplicationContext context,
                final RestDocumentationContextProvider provider) {
+        MockMvcOperationPreprocessorsConfigurer configuration = documentationConfiguration(provider)
+                .operationPreprocessors()
+                .withRequestDefaults(prettyPrint())
+                .withResponseDefaults(prettyPrint());
+
         this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
-                .apply(MockMvcRestDocumentation.documentationConfiguration(provider))
+                .apply(configuration)
                 .alwaysDo(MockMvcResultHandlers.print())
                 .alwaysDo(restDocs)
                 .addFilters(new CharacterEncodingFilter("UTF-8", true))
