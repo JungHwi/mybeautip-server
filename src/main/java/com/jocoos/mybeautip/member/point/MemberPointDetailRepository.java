d112cf773a23a61421f65f4e42f6df33f5093f61
KimJaeHun <kjh96@jocoos.com>
2022-09-07 12:06:38 +0900

    MYBEAUTIP583 - [style] 안쓰는 repository method 삭제

--- src/main/java/com/jocoos/mybeautip/member/point/MemberPointDetailRepository.java
+++ src/main/java/com/jocoos/mybeautip/member/point/MemberPointDetailRepository.java
@@ -2,14 +2,13 @@ package com.jocoos.mybeautip.member.point;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 public interface MemberPointDetailRepository extends JpaRepository<MemberPointDetail, Long> {
+
     Optional<MemberPointDetail> findTopByMemberIdAndStateInAndExpiryAtAfterOrderByIdDesc(Long memberId, List<Integer> states, Date now);
 
     @Query("select min(mpd.id) as id, sum(mpd.point) as pointSum, max(mpd.expiryAt) as expiryAt from MemberPointDetail mpd where mpd.parentId = ?1 group by mpd.parentId")
@@ -21,23 +20,9 @@ public interface MemberPointDetailRepository extends JpaRepository<MemberPointDe
 
     List<MemberPointDetail> findByParentIdAndState(Long parentId, int state);
 
-    @Query("select sum(mpd.point) as pointSum, max(mpd.expiryAt) as expiryAt from MemberPointDetail mpd where mpd.memberPointId = ?1 group by mpd.memberPointId")
-    Optional<MemberPointSum> getSumByMemberPointId(Long memberPointId);
-
-    @Query("select mpd from MemberPointDetail mpd where mpd.memberId = ?1 and mpd.state = ?2 and mpd.id > ?3 and expiry_at >= now() order by mpd.createdAt asc")
-    List<MemberPointDetail> getAllEarnedPoints(Long memberId, int state, Long cursor);
-
-    @Query("SELECT mpd " +
-            "FROM MemberPointDetail mpd " +
-            "WHERE mpd.memberId = ?1 " +
-            "   AND mpd.state IN (?2) " +
-            "   AND mpd.id >= ?3 " +
-            "ORDER BY mpd.id")
-    List<MemberPointDetail> getAvailablePoint(long memberId, List<Integer> state, Long pointDetailId);
-
     List<MemberPointDetail> findByOrderIdAndStateAndExpiryAtAfter(Long orderId, int state, Date now);
 
     List<MemberPointDetail> findAllByMemberIdAndStateAndParentIdIsNullOrderByIdAsc(long memberId, int state);
-    Optional<MemberPointDetail> findTopByMemberIdOrderByIdDesc(long memberId);
+
     List<MemberPointDetail> findAllByMemberIdAndMemberPointIdGreaterThanEqualAndStateOrderById(long memberId, long cursor, int state);
 }
--- src/main/java/com/jocoos/mybeautip/member/point/MemberPointRepository.java
+++ src/main/java/com/jocoos/mybeautip/member/point/MemberPointRepository.java
@@ -3,7 +3,6 @@ package com.jocoos.mybeautip.member.point;
 import com.jocoos.mybeautip.domain.point.code.ActivityPointType;
 import com.jocoos.mybeautip.member.Member;
 import com.jocoos.mybeautip.member.order.Order;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -17,13 +16,9 @@ import java.util.Set;
 
 public interface MemberPointRepository extends JpaRepository<MemberPoint, Long> {
 
-    List<MemberPoint> findByIdIn(List<Long> ids);
-
     Slice<MemberPoint> findByMemberAndCreatedAtBefore(Member member, Date createdAt, Pageable page);
 
     List<MemberPoint> findByMemberAndState(Member member, int state);
-
-    List<MemberPoint> findByStateAndCreatedAtBeforeAndEarnedAtIsNull(int state, Date createdAt);
 
     List<MemberPoint> findByStateInAndExpiryAtBeforeAndExpiredAtIsNull(List<Integer> states, Date earnedAt);
 
@@ -42,24 +37,7 @@ public interface MemberPointRepository extends JpaRepository<MemberPoint, Long> 
             "   AND memberPoint.state in :state")
     Slice<MemberPoint> findByMemberIdAndStateIn(long memberId, Set<Integer> state, Long cursor, Pageable pageable);
 
-    Page<MemberPoint> findByMemberIdAndState(Long memberId, int state, Pageable pageable);
-
     Optional<MemberPoint> findByMemberAndOrderAndPointAndState(Member member, Order order, int point, int state);
-
-    @Query("SELECT memberPoint " +
-            "FROM MemberPoint AS memberPoint " +
-            "WHERE memberPoint.member.id = :memberId " +
-            "   AND memberPoint.id >= :cursor " +
-            "   AND memberPoint.state in :state " +
-            "ORDER BY memberPoint.id ")
-    List<MemberPoint> getAvailablePoint(
-            @Param("memberId") long memberId,
-            @Param("state") Set<Integer> state,
-            @Param("cursor") Long cursor);
-
-
-    // admin api
-    Page<MemberPoint> findByStateAndOrderIsNull(int state, Pageable pageable);
 
     @Query("SELECT memberPoint " +
             "FROM MemberPoint as memberPoint " +
@@ -96,5 +74,4 @@ public interface MemberPointRepository extends JpaRepository<MemberPoint, Long> 
             @Param("types") Set<ActivityPointType> types,
             @Param("member") Member member);
 
-    Optional<MemberPoint> findByMemberAndActivityTypeAndActivityDomainId(Member member, ActivityPointType type, long domainId);
 }
