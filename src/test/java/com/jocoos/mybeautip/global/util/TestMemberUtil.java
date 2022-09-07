8d79f447d74c0416cb08afadff97157d02b3d505
KimJaeHun <kjh96@jocoos.com>
2022-09-07 12:06:59 +0900

    MYBEAUTIP583 - [test] 포인트 로직 테스트 작성

--- src/test/java/com/jocoos/mybeautip/global/util/TestMemberUtil.java
+++ src/test/java/com/jocoos/mybeautip/global/util/TestMemberUtil.java
@@ -1,9 +1,31 @@
-package com.jocoos.mybeautip.util;
+package com.jocoos.mybeautip.global.util;
 
 import com.jocoos.mybeautip.member.Member;
+import com.jocoos.mybeautip.member.MemberRepository;
 import com.jocoos.mybeautip.restapi.dto.SignupRequest;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestComponent;
 
-public class MemberUtil {
+@TestComponent
+public class TestMemberUtil {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Getter
+    private Member member;
+
+    public void defaultTestSetting() {
+        final long memberId = 0L;
+        final String socialId = "testSocialId";
+        member = memberRepository.save(defaultMember(memberId, socialId));
+    }
+
+    public void defaultTestEnd() {
+        memberRepository.delete(member);
+    }
 
     public static Member defaultMember(Long memberId, String socialId) {;
         final String grantType = "naver";
