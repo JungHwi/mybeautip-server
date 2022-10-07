86a447083431624caef60da412be4b1258971d91
KimJaeHun <kjh96@jocoos.com>
2022-10-07 11:52:33 +0900

    MYBEAUTIP826 - [fix] date utc offset 지정해줌
    UTC로 고정, 아니면 시간값이 db와 맞지 않게 들어감

--- src/main/java/com/jocoos/mybeautip/domain/search/vo/KeywordSearchCondition.java
+++ src/main/java/com/jocoos/mybeautip/domain/search/vo/KeywordSearchCondition.java
@@ -4,8 +4,9 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
@@ -17,6 +18,7 @@ import static com.jocoos.mybeautip.global.constant.LocalDateTimeConstant.ZONE_DA
 @RequiredArgsConstructor
 public class KeywordSearchCondition {
     private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(ZONE_DATE_TIME_MILLI_FORMAT);
+    private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
     private final String keyword;
 
     private final String cursor;
@@ -32,15 +34,12 @@ public class KeywordSearchCondition {
     }
 
     public Date cursorDate() {
-        final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.sss");
-        return cursorToDate(simpleDateFormat);
+        return StringUtils.isBlank(cursor) ? null : cursorToDate();
     }
 
-    private Date cursorToDate(SimpleDateFormat simpleDateFormat) {
-        try {
-            return StringUtils.isBlank(cursor) ? new Date() : simpleDateFormat.parse(cursor);
-        } catch (ParseException e) {
-            throw new IllegalArgumentException("Wrong Type Of Date" + cursor, e);
-        }
+    private Date cursorToDate() {
+        LocalDateTime localDateTime = LocalDateTime.parse(cursor, dateFormatter);
+        Instant instant = localDateTime.toInstant(ZoneOffset.UTC);
+        return Date.from(instant);
     }
 }
