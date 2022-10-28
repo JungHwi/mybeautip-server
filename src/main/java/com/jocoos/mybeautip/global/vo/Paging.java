3ea0daa4059a5678c2efd52187cd9ba8646e8e69
KimJaeHun <kjh96@jocoos.com>
2022-10-28 09:25:52 +0900

    [feat] Paging - NoOffset 추가
    서비스 로직에선 페이징 로직이 cursor-based-pagination이고 어드민 로직에선 offset pagination을 사용하기 때문에 둘 다 포함하는 클래스로 변경
    인터페이스를 활용해 두 로직을 분리할 수도 있으나 페이지네이션 로직이 2개밖에 없고 많은 곳에서 활용되지 않기 때문에 정적 팩토리 메소드를 활용해 한 클래스 내에서 구현
    offset pagination을 커서를 활용해 구현할 수도 있단 얘기를 들은적이 있어 추후 리팩토링할 수도 있음

--- src/main/java/com/jocoos/mybeautip/global/vo/Paging.java
+++ src/main/java/com/jocoos/mybeautip/global/vo/Paging.java
@@ -1,17 +1,29 @@
 package com.jocoos.mybeautip.global.vo;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.time.ZonedDateTime;
+
 @Getter
-@RequiredArgsConstructor
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class Paging {
     private final Long page;
     private final Long size;
 
-    public Paging(long size) {
-        this.page = null;
-        this.size = size;
+    private final ZonedDateTime cursor;
+
+    public static Paging onlyLimit(long size) {
+        return new Paging(null, size, null);
+    }
+
+    public static Paging page(Long page, Long size) {
+        return new Paging(page, size, null);
+    }
+
+    public static Paging noOffset(ZonedDateTime cursor, Long size) {
+        return new Paging(null, size, cursor);
     }
 
     public long getOffset() {
