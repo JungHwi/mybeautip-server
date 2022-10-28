b2d7a230afc27c2ed68e900fab1f06323a2a5e1f
KimJaeHun <kjh96@jocoos.com>
2022-10-28 15:57:51 +0900

    [test] link_parameter to optional
    link null일 경우 메인탭

--- src/test/java/com/jocoos/mybeautip/domain/placard/api/front/PlacardControllerTest.java
+++ src/test/java/com/jocoos/mybeautip/domain/placard/api/front/PlacardControllerTest.java
@@ -32,7 +32,7 @@ class PlacardControllerTest extends RestDocsTestSupport {
                         fieldWithPath("[].color").type(JsonFieldType.STRING).description("플랜카드 색깔 정보"),
                         fieldWithPath("[].placard_link").type(JsonFieldType.OBJECT).description("플랜카드 클릭 시 연결 정보"),
                         fieldWithPath("[].placard_link.link_type").type(JsonFieldType.STRING).description(generateLinkCode((PLACARD_LINK_TYPE))),
-                        fieldWithPath("[].placard_link.parameter").type(JsonFieldType.STRING).description("연결 정보 파라미터 (이벤트나 비디오 ID, null일 경우 해당 탭으로")
+                        fieldWithPath("[].placard_link.parameter").type(JsonFieldType.STRING).description("연결 정보 파라미터 (이벤트나 비디오 ID, null일 경우 해당 탭으로").optional()
                 )));
     }
 
