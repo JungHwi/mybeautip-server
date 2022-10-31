6edb489f67feafc9e7df8dd9db50d71c418290ab
KimJaeHun <kjh96@jocoos.com>
2022-10-31 11:27:03 +0900

    [fix] 정책 확정 전까지 커뮤니티 목록 조회 주석 처리

--- src/main/java/com/jocoos/mybeautip/domain/community/api/admin/AdminCommunityController.java
+++ src/main/java/com/jocoos/mybeautip/domain/community/api/admin/AdminCommunityController.java
@@ -1,9 +1,7 @@
 package com.jocoos.mybeautip.domain.community.api.admin;
 
-import com.jocoos.mybeautip.domain.community.dto.AdminCommunityResponse;
 import com.jocoos.mybeautip.domain.community.dto.CommunityCategoryResponse;
 import com.jocoos.mybeautip.domain.community.service.AdminCommunityService;
-import com.jocoos.mybeautip.global.wrapper.PageResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,8 +22,23 @@ public class AdminCommunityController {
         return ResponseEntity.ok(service.getCategories());
     }
 
-    @GetMapping("/community")
-    public ResponseEntity<PageResponse<List<AdminCommunityResponse>>> getCommunities() {
-        return ResponseEntity.ok(service.getCommunities());
-    }
+//    @GetMapping("/community")
+//    public ResponseEntity<PageResponse<List<AdminCommunityResponse>>> getCommunities(
+//            @RequestParam(required = false) Long categoryId,
+//            @RequestParam(required = false, defaultValue = "1") Long page,
+//            @RequestParam(required = false, defaultValue = "10") Long size,
+//            @RequestParam(required = false, defaultValue = "createdAt") String sort,
+//            @RequestParam(required = false, defaultValue = "DESC") String order,
+//            @RequestParam(required = false) String search,
+//            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startAt,
+//            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endAt) {
+//
+//        CommunitySearchCondition condition = CommunitySearchCondition.builder()
+//                .paging(Paging.page(page, size))
+//                .sort(new Sort(sort, order))
+//                .searchKeyword(SearchKeyword.from(search, startAt, endAt, ZoneId.of("Asia/Seoul")))
+//                .build();
+//
+//        return ResponseEntity.ok(service.getCommunities(categoryId, condition));
+//    }
 }
