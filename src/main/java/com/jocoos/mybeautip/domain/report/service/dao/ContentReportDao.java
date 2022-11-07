9f8e65de9c61f35c4e31f8a41f511079938209c9
KimJaeHun <kjh96@jocoos.com>
2022-11-07 10:54:03 +0900

    MYBEAUTIP980 - [feat] native query로 union 쿼리 작성
    jqql은 기본적으로 유니온을 지원하지 않음 querydsl로 유니온 쿼리를 작성하려 했으나 유니온 쿼리에 대한 레퍼런스가 없어 네이티브 쿼리로 작성

---
+++ src/main/java/com/jocoos/mybeautip/domain/report/service/dao/ContentReportDao.java
@@ -0,0 +1,67 @@
+package com.jocoos.mybeautip.domain.report.service.dao;
+
+import com.jocoos.mybeautip.domain.member.dto.AdminMemberReportResponse;
+import com.jocoos.mybeautip.domain.member.dto.MemberIdAndUsernameResponse;
+import com.jocoos.mybeautip.domain.member.persistence.repository.NativeContentReportRepository;
+import com.jocoos.mybeautip.global.util.date.ZonedDateTimeUtil;
+import com.jocoos.mybeautip.global.wrapper.PageResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Service
+public class ContentReportDao {
+
+    private final NativeContentReportRepository repository;
+
+    @Transactional(readOnly = true)
+    public PageResponse<AdminMemberReportResponse> getAllAccusedBy(Long accusedId, Pageable pageable) {
+        Page<Map<String, Object>> page = repository.unionAllContentReport(accusedId, pageable);
+
+        List<AdminMemberReportResponse> response = page.getContent()
+                .stream()
+                .map(this::toResponse)
+                .toList();
+
+        return new PageResponse<>(page.getTotalElements(), response);
+    }
+
+    private AdminMemberReportResponse toResponse(Map<String, Object> map) {
+        return new AdminMemberReportResponse(
+                getId(map),
+                getAccuserResponse(map),
+                getReason(map),
+                getReportedAt(map));
+    }
+
+    private String getId(Map<String, Object> map) {
+        return getStringValue(map, "type") + map.get("id");
+    }
+
+    private MemberIdAndUsernameResponse getAccuserResponse(Map<String, Object> map) {
+        return new MemberIdAndUsernameResponse(getAccuserId(map), getStringValue(map, "accuserUsername"));
+    }
+
+    private long getAccuserId(Map<String, Object> map) {
+        return Long.parseLong(getStringValue(map, "accuserId"));
+    }
+
+    private String getReason(Map<String, Object> map) {
+        return getStringValue(map, "reason");
+    }
+
+    private ZonedDateTime getReportedAt(Map<String, Object> map) {
+        return ZonedDateTimeUtil.toUTCZoned(getStringValue(map, "createdAt"));
+    }
+
+    private String getStringValue(Map<String, Object> map, String key) {
+        return String.valueOf(map.get(key));
+    }
+}
