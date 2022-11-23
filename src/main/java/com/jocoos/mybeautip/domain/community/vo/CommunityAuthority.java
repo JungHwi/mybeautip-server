935b3d187ab2b7d66cccb466a1376c1797cd3139
KimJaeHun <kjh96@jocoos.com>
2022-11-23 10:38:45 +0900

    MYBEAUTIP1273 - [feat] role, communityAuthority 클래스 작성
    회원의 권한 Role enum과 해당 커뮤니티 카테고리에 쓰거나 읽을 권한이 있는지 파악하는 책임의 CommunityAuthority 클래스

---
+++ src/main/java/com/jocoos/mybeautip/domain/community/vo/CommunityAuthority.java
@@ -0,0 +1,35 @@
+package com.jocoos.mybeautip.domain.community.vo;
+
+import com.jocoos.mybeautip.domain.member.code.Role;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Set;
+
+import static com.jocoos.mybeautip.domain.member.code.Role.*;
+
+@Getter
+@RequiredArgsConstructor
+public class CommunityAuthority {
+
+    private final Set<Role> writeAuth;
+    private final Set<Role> readAuth;
+
+    public CommunityAuthority(Set<Role> auth) {
+        this.writeAuth = auth;
+        this.readAuth = auth;
+    }
+
+    public CommunityAuthority(Role role, Set<Role> roles) {
+        this.writeAuth = Set.of(role);
+        this.readAuth = roles;
+    }
+
+    public static CommunityAuthority defaultAuth() {
+        return new CommunityAuthority(Set.of(USER, ADMIN), ALL);
+    }
+
+    public boolean canWrite(Role role) {
+        return writeAuth.contains(role);
+    }
+}
