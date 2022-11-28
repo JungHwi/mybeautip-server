2720a4b2435a47826a58f65f2a3619dedc853ed8
KimJaeHun <kjh96@jocoos.com>
2022-11-28 16:17:35 +0900

    MYBEAUTIP-1178 [feat] json nullable 추가

---
+++ src/main/java/com/jocoos/mybeautip/global/util/JsonNullableUtils.java
@@ -0,0 +1,16 @@
+package com.jocoos.mybeautip.global.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.openapitools.jackson.nullable.JsonNullable;
+
+import java.util.function.Consumer;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class JsonNullableUtils {
+    public static <T> void changeIfPresent(JsonNullable<T> nullable, Consumer<T> consumer) {
+        if (nullable.isPresent()) {
+            consumer.accept(nullable.get());
+        }
+    }
+}
