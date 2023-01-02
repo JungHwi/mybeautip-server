f5837d08cbb20d43e953c85ba259e14543d2194d
breeze <breeze@jocoos.com>
2023-01-02 09:53:26 +0900

    이상한 테스트 삭제

--- src/test/java/com/jocoos/mybeautip/sadadsad.java
+++ src/test/java/com/jocoos/mybeautip/sadadsad.java
@@ -1,12 +1,10 @@
 package com.jocoos.mybeautip;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-@SpringBootTest
-public class sadadsad extends OldTest {
+//@SpringBootTest
+public class sadadsad /*extends OldTest*/ {
 
-    @Test
-    void sad() {
-
-    }
+//    @Test
+//    void sad() {
+//
+//    }
 }
