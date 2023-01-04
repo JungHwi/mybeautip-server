91a2cf54062ab0d102e995cde9ffcec78ebafb10
KimJaeHun <kjh96@jocoos.com>
2023-01-04 10:06:32 +0900

    MYBEAUTIP-1686 [feat] spring data rest build gradle 제거 및 Excerpt 클래스 제거

--- src/main/java/com/jocoos/mybeautip/admin/AdminMemberRepository.java
+++ src/main/java/com/jocoos/mybeautip/admin/AdminMemberRepository.java
@@ -1,20 +1,10 @@
 package com.jocoos.mybeautip.admin;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface AdminMemberRepository extends JpaRepository<AdminMember, String> {
-
-    Page<AdminMember> findByMemberLinkAndMemberDeletedAtIsNullOrderByStoreIdDesc(int link, Pageable pageable);
-
-    Page<AdminMember> findByMemberLinkAndMemberDeletedAtIsNotNullOrderByStoreIdDesc(int link, Pageable pageable);
-
-    Page<AdminMember> findByMemberLinkAndMemberDeletedAtIsNullOrderByStoreLikeCountDesc(int link, Pageable pageable);
-
-    Page<AdminMember> findByMemberLinkAndMemberDeletedAtIsNullOrderByStoreGoodsCountDesc(int link, Pageable pageable);
 
     boolean existsByEmail(String email);
 
